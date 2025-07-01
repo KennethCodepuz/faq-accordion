42219748bb7118b5d6e3e2131b1f6181cd0499de
KennethCodepuz <kennethseancorpuz14@gmail.com>
2025-07-01 23:16:36 +0800

    added all the images needed for the backgrounds and icons. installed tailwind css added the background for the mobile and desktop created file FaqAccordion and TopBackground

---
+++ src/components/TopBackground.jsx
@@ -0,0 +1,6 @@
+import '../App.css'
+
+export default function TopBackground() {
+
+  return <div className='bg-(image:--background-mobile) h-[232px] w-full bg-no-repeat bg-cover border-red-500 md:bg-(image:--background-desktop)'></div>
+}
